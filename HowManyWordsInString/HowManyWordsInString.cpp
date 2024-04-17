2f48d9e9b268e932aee11d693fd7ae34043ac5e9
MkGodh <fenryrvla@yandex.ru>
2024-04-17 21:16:16 +0300

    Добавьте файлы проекта.

---
+++ HowManyWordsInString/HowManyWordsInString.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+#include <string>
+#include <cstring>
+#include <cmath>
+#include <iomanip>
+#include <math.h>
+#include <vector>
+
+using namespace std;
+int main() {
+    string s;
+    cout << "Input string: "; getline(cin, s);
+
+    bool hasChar = false;
+    int hasWord = 0;
+    bool hWord = false;
+    int count = 0;
+    for (int i = 0; i < s.length(); i++) {
+
+        if (s[i] >= '!' && s[i] <= '~') {
+            if (!hWord) {
+                hWord = true;
+            }
+
+        }
+        else {
+            if (hWord) {
+                hWord = false;
+                count++;
+            }
+
+        }
+
+
+    }
+
+    if (hWord) {
+        count++;
+        cout << count << "\n";
+    }
+    else if (!hWord) {
+        cout << count << "\n";
+    }
+
+}
